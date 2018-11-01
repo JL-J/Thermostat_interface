225b5e5a216088989eb297a45513db25d7705c77
Judith Lloyd-Jones <j.r.lloydjones@gmail.com>
2018-11-01 16:32:05 +0000

    added css

--- src/interface.js
+++ src/interface.js
@@ -30,7 +30,7 @@ $(document).ready(function() {
   })
 
   function updateTemperature() {
-    $('#temperature').text(thermostat.temperature);
+    $('#temperature').text(thermostat.temperature + '°C');
     $('#temperature').attr('class', thermostat.currentUsage());
   };
 
